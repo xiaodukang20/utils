98d2c6b242e7b95998b35f2091d17d4bdf2019ab
malinjie <xiaodukang20@126.com>
2021-08-23 16:57:57 +0800

    init

---
+++ src/Mime/MimeDetectorException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: runs
+ * Date: 19-2-27
+ * Time: 下午4:30
+ */
+
+namespace Xiaodukang20\Utils\Mime;
+
+/**
+ * Class MimeDetectorException
+ *
+ * @package Xiaodukang20\Utils\Mime
+ */
+class MimeDetectorException extends \Exception
+{
+
+}
